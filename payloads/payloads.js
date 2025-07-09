470ed2c552881109c60906f75f8157cba6527498
Yaman Alhusein Alosman <yamanosman1st@gmail.com>
2025-07-09 13:22:34 +0200

    cleanup

--- payloads/payloads.js
+++ payloads/payloads.js
@@ -77,8 +77,6 @@ function Loadpayloadonline(PLfile) {
     window.payload_path = PLfile;
     setTimeout(() => {
         sessionStorage.setItem('jbsuccess', 1);
-        document.getElementById('jailbreak').style.display = 'flex';
-        document.getElementById('loader').style.display = 'none';
         window.location.reload();
     }, 3000); // 3 seconds delay
 }
